394e3f23cfa7b7dfff3df8fc1ddf6cb31b2c3792
Dmitry-dmv <bliznetsov.an@yandex.ru>
2021-06-28 17:28:11 +0300

    Initial commit

---
+++ src/chernyh/company/Main.java
@@ -0,0 +1,135 @@
+package chernyh.company;
+
+import java.util.GregorianCalendar;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        class Study {
+
+            private String course;
+
+
+
+                private String course;
+
+                public Study(String course) {
+                }
+
+                //TODO
+
+            public Study(String course, String course1) {
+                this.course = course;
+                this.course = course1;
+            }
+
+            public String printCourse() {
+
+                    Study = new Study(course);
+                    return this.course;
+                    course = "Изучение Java это просто";
+                    System.out.println(course);
+                }
+            }
+
+            class JavaProgram {
+                public void main(String[] args) {
+
+                    //TODO
+                }
+            }
+        }
+    }
+}
+public class House
+
+        private double aptSquare;
+        private GregorianCalendar lifetime;
+        private String name;
+
+        public House(double aptSquare, GregorianCalendar lifetime, String name);
+
+        public static double aptSquare  getInstance() {
+            return ;
+
+        }
+
+        public static GregorianCalendar lifetime, getInstance() {
+            return ;
+        }
+
+public static String name getInstance(){
+        return ;
+        }
+public void setLifetime(GregorianCalendar lifetime) {
+        GregorianCalendar currentTime = new GregorianCalendar();
+        if(lifetime.get(Calendar.YEAR) < currentTime.get(Calendar.YEAR)) {
+        this.lifetime = lifetime;
+        }else{
+        throw new IllegalArgumentException();
+public String toString() {
+final StringBuilder sb = new StringBuilder("House{");
+        sb.append(", Name='").append(Name).append('\'');
+        sb.append(", aptSquare=").append(aptSquare);
+        return sb.toString();
+        }
+
+public class Tree
+        String name
+        int age;
+         String is the tree alive;
+        public Tree ( String name; int age; ) {
+          this name = name;
+          this age = age
+public Tree ( String name; int age;String is the tree alive )
+        this name = name;
+        this age = age;
+        this String is the tree alive;
+public Tree() {
+        };
+public static void main(String[]args){
+        String name = new name ("ELKA")
+        int age = new age ("150")
+        System.out.print(name);
+        System.out.print(age);
+}
+
+        }
+
+public class Aircraft{
+
+    int wind weight = new wind weight;
+    System.out.println(wind weight)
+
+    public class wing {
+            int wing weight;
+            public String getInfo() {
+                return this.wing weight;
+
+        }
+}
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// write your code here
+
+
